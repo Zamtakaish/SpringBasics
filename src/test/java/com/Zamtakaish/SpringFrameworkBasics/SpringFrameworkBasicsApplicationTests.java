2ab70a4f9f512efe0c5094686de874099f53ec91
Anton Ziuzikov <zuzikov.anton@gmail.com>
2021-04-29 19:22:46 +0300

    init: initial commit

---
+++ src/test/java/com/Zamtakaish/SpringFrameworkBasics/SpringFrameworkBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zamtakaish.SpringFrameworkBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
